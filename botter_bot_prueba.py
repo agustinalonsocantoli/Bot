b3b9283e403ae40987524913fbf0d085512be93a
agustinalonsocantoli <agustinalonsocantoli@gmail.com>
2022-10-06 11:48:03 +0200

    Bot dice hola, brinda dia y hora actual

--- botter_bot_prueba.py
+++ botter_bot_prueba.py
@@ -1,16 +1,49 @@
 from datetime import datetime
+from lib2to3.pgen2 import token
 from tkinter import Button
 import types
 import telebot
 import requests
 
-bot = telebot.TeleBot("5420608268:AAHmtRiwizz4Mpmbuy2GQEuHt4hZhT5Wsp0")     #Token de indetificación
 
-hora = datetime.today() #Creo variable de current date
+#Token de indetificación
+token = "5420608268:AAHmtRiwizz4Mpmbuy2GQEuHt4hZhT5Wsp0"
+bot = telebot.TeleBot(token)     
 
 
-@bot.message_handler(commands=["hora"])  #Se estable comando '/hora'
+#Creo variable de current date
+fecha_hora = datetime.today() 
+
+def informar_dia():
+
+    # crear variable para el dia de semana
+    dia_semana = fecha_hora.weekday()
+
+    # diccionario con nombres de dias
+    calendario = {0: 'Lunes',
+                  1: 'Martes',
+                  2: 'Miércoles',
+                  3: 'Jueves',
+                  4: 'Viernes',
+                  5: 'Sábado',
+                  6: 'Domingo'}
+
+    # decir el dia de la semana
+    dia = calendario[dia_semana]
+    return dia
+
+
+#Se estable comando '/hora'
+@bot.message_handler(commands=["help", "start"])  
 def enviar(message):
-    bot.reply_to(message, hora.time().strftime('%H:%M')) #Se aplica formato a la respuesta
+    #Se aplica formato a la respuesta
+    bot.reply_to(message, f"Hola!!\nHoy es {informar_dia()} {fecha_hora.strftime('%d')} del {fecha_hora.strftime('%m')} y son las {fecha_hora.strftime('%H:%M:%S')}")
+
+
+@bot.message_handler(func=lambda message:True) 
+def mensaje(massage):
+     bot.reply_to(massage, massage.text) 
+
     
-bot.infinity_polling() #Mantiene actividad del bot
+#Mantiene actividad del bot
+bot.polling() 
