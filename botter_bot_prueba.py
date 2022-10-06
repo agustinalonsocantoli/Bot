4f31f171bda6c509e812564eb31f32f303e40148
agustinact <agustin.act@outlook.com>
2022-10-06 02:48:59 -0300

    Pruebas

--- botter_bot_prueba.py
+++ botter_bot_prueba.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+from tkinter import Button
+import types
 import telebot
+import requests
 
-bot = telebot.TeleBot("5420608268:AAHmtRiwizz4Mpmbuy2GQEuHt4hZhT5Wsp0")
+bot = telebot.TeleBot("5420608268:AAHmtRiwizz4Mpmbuy2GQEuHt4hZhT5Wsp0")     #Token de indetificación
 
-@bot.message_handler(commands=["help", "start"])
+hora = datetime.today() #Creo variable de current date
 
+
+@bot.message_handler(commands=["hora"])  #Se estable comando '/hora'
 def enviar(message):
-    bot.reply_to(message, "Hola, ¿Cómo estas?")
+    bot.reply_to(message, hora.time().strftime('%H:%M')) #Se aplica formato a la respuesta
     
-bot.polling()
+bot.infinity_polling() #Mantiene actividad del bot
